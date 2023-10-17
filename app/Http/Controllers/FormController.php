78b5bff2505de494352fe5a07029c8b31f6f1a57
Fathan Abi <abikarami60@gmail.com>
2023-10-17 22:05:54 +0700

    init

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    public function index()
+    {
+        $datas = User::all();
+
+        return view('detailAccount', ["datas" => $datas]);
+    }
+    public function store(Request $request)
+    {
+        
+        $request->validate([
+            'Name' => 'required',
+            'Phone' => 'required',
+            'Email' => 'required',
+            'Password' => 'required',
+            'Confirm' => 'required',
+            'gambar' => 'required|max:20480|mimes:jpg,png,jpeg'
+        ]);
+
+        
+        
+
+        $extension = $request->file('gambar')->extension();
+        $filename = date('Y-m-d-H-i-s') . "." . $extension;
+
+        $request->file('gambar')->storeAs('public/images', $filename);
+
+        $request['gambar'] = $filename;
+        User::create([
+            "Name" => $request->Name,
+            "Phone" => $request->Phone,
+            "Email" => $request->Email,
+            "Password" => $request->Password,
+            "Confirm" => $request->Confirm,
+            "Gambar" => $filename,
+        ]);
+
+        
+        return redirect('/allForm');
+    }
+}
